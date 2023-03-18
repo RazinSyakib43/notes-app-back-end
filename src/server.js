b9152127b5b2eae2197677c94f2d7e44bd8d4ec3
Razin Syakib <muhammadrazin14@gmail.com>
2023-03-18 19:33:46 +0700

    done

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable eqeqeq */
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV != 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
